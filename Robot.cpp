3670e12c2f0121c77ceba858f6647caa46838b85
Numeri <Numeri@users.noreply.github.com>
2016-01-26 11:28:41 -0500

    Scaled back speeds, made it repeat

--- Robot.cpp
+++ Robot.cpp
@@ -11,12 +11,15 @@ void Robot::OperatorControl() //teleop code
 {
 	robotDrive.SetSafetyEnabled(false);
 
-	robotDrive.TankDrive(1,1);
-	Wait(0.5);
-	robotDrive.TankDrive(-1,-1);
-	Wait(1);
-	robotDrive.TankDrive(0, 1);
-	Wait(2);
+	while(IsOperatorControl() && IsEnabled())
+	{
+		robotDrive.TankDrive(0.3,0.3);
+		Wait(1);
+		robotDrive.TankDrive(-0.3,-0.3);
+		Wait(1);
+		robotDrive.TankDrive(0, 0.2);
+		Wait(2);
+	}
 }
 
 START_ROBOT_CLASS(Robot);
